8b23d0df19757a6843553f933ba148fc8a2d4eff
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-05-12 12:40:35 -0300

    Add relacionamentos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from ..funcionarios.models import Funcionarios
 
 # Create your models here.
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(Funcionarios, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return "{}".format(self.descricao)
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from ..funcionarios.models import Funcionarios
 
 # Create your models here.
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=255)
+    funcionario = models.ForeignKey(Funcionarios, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return "{}".format(self.motivo)
